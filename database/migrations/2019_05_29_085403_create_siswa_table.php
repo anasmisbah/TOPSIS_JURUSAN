a5d144e0b68da65132fd515409866a9c9d525967
AnasMisbahuddin <anas.misbahuddin4869@gmail.com>
2019-05-29 16:59:14 +0800

    nambah migrasi , model dan controller

---
+++ database/migrations/2019_05_29_085403_create_siswa_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nis');
+            $table->string('nama');
+            $table->string('kelas');
+            $table->string('jurusan');
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswa');
+    }
+}
